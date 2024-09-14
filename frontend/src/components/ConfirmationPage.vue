7d6c7867e8616735f7ec4e4dbb62e19ad43866ff
Daniel Mora Rodriguez <dmorarod16@gmail.com>
2024-09-13 21:06:31 -0600

    Added custom files for bootstrap, integrated css partially

--- frontend/src/components/ConfirmationPage.vue
+++ frontend/src/components/ConfirmationPage.vue
@@ -1,35 +1,45 @@
 <template>
-  <header class="header">
-    <nav class="navbar bg-body-tertiary">
+  <div class="d-flex flex-column" style="">
+    <nav class="navbar bg-primary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Feria del Emprendedor</a>
+        <a class="navbar-brand fw-bold ff-poppins" href="#">Feria del Emprendedor</a>
       </div>
     </nav>
-  </header>
-  <div class="container mt-5 mb-5">
-    <div class="container mt-5">
-      <h1 class="display-5 text-center">Confirmación</h1>
-      <div class="container">
-        Se le ha enviado un correo con un código de 6 digitos para confirmar el registro de su nueva cuenta. Solamente debe colocarlo abajo y darle al botón <strong>Crear Cuenta</strong>.
+    <div class="container-fluid bg-secondary py-5 h-100">
+      <div class="container bg-light pt-4 pb-4 rounded-4 h-100 d-flex flex-column mb-3">
+        <h1 class="display-6 text-center fw-bold ff-poppins">Confirmación</h1>
+        <div class="ff-poppins container">
+          Se le ha enviado un correo con un código de 6 digitos para confirmar el registro de su nueva cuenta. Solamente debe colocarlo abajo y darle al botón <strong>Crear Cuenta</strong>.
+        </div>
+        <form action="" class="text-center">
+          <label class="form-label ff-poppins" for=""><strong>Código de Confirmación</strong></label>
+          <br>
+          <input
+            class="form-control-sm rounded-2 border-0 bg-secondary p-2 mt-1 text-center fs-5"
+            type="text"
+            placeholder="xxx-xxx"
+            maxlength="6" />
+        </form>
+        <div class="container mt-5 mb-4 ff-poppins text-center">
+          En caso de que no le haya llegado el código, utilize el botón inferior de renviar código.
+        </div>
+        <form action="" class="text-center d-grid gap-2">
+          <input
+            class="btn btn-secondary ff-poppins"
+            type="button"
+            value="Renviar Código"
+          >
+          <input class="btn fw-bold btn-primary ff-poppins"
+            type="button"
+            value="Crear Cuenta"
+          >
+        </form>
       </div>
-      <br>
-      <form action="" class="text-center">
-        <label class="form-label" for=""><strong>Código de Confirmación</strong></label>
-        <br>
-        <input class="form-control-sm" type="text">
-      </form>
-      <div class="container mt-5 mb-4">
-        En caso de que no le haya llegado el código, utilize el botón inferior de renviar código.
-      </div>
-      <form action="" class="text-center d-grid gap-2">
-        <input class="btn btn-secondary" type="button" value="Renviar Código">
-        <input class="btn btn-primary" type="button" value="Crear Cuenta">
-      </form>
     </div>
+    <footer class="text-center p-2 fst-italic bg-ternary text-light ff-poppins">
+      @Copyright BichiWare Solutions 2024
+    </footer>
   </div>
-  <footer class="text-center mt-3 p-2 fs-6 fst-italic bg-secondary">
-    @Copyright BichiWare Solutions 2024
-  </footer>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import bootstrap from 'bootstrap'
+// import 'bootstrap/dist/css/custom.css'
+import '../scss/custom.css'
 
-createApp(App).mount('#app')
+createApp(App).use(bootstrap).mount('#app')
