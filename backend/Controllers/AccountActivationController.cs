e3549147b99385c689a915424e269058239f7c95
Daniel Mora Rodriguez <dmorarod16@gmail.com>
2024-09-18 13:31:43 -0600

    Added account activation controller and handler

---
+++ backend/Controllers/AccountActivationController.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using backend.Handlers;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountActivationController : Controller
+    {
+        private readonly AccountActivationHandler accountActivationHandler;
+
+        public AccountActivationController()
+        {
+            this.accountActivationHandler = new AccountActivationHandler();
+        }
+
+        [HttpGet]
+        public void GetHashedConfirmationCode()
+        {
+
+        }
+    }
+}
