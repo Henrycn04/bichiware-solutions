4a184fd37c37def67bf8adb7b490a3452a419c89
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-09-29 02:41:48 -0600

    Added all necessary elements and initial development of the company's inventory view.

---
+++ backend/Controllers/CompanyProductsController.cs
@@ -0,0 +1,45 @@
+﻿using backend.Handlers;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompanyProductsController : ControllerBase
+    {
+        private CompanyProductsHandler _companyProductsHandler;
+
+        public CompanyProductsController()
+        {
+            _companyProductsHandler = new CompanyProductsHandler();
+        }
+
+        // Endpoint para obtener productos no perecederos
+        [HttpGet("non-perishable")]
+        public IActionResult ObtenerProductosNoPerecederos([FromQuery] int empresa)
+        {
+
+            var productos = _companyProductsHandler.ObtenerProductosNoPerecederos(empresa);
+            if (productos == null || !productos.Any())
+            {
+                return Ok();
+            }
+            return Ok(productos);
+        }
+
+        // Endpoint para obtener productos perecederos
+        [HttpGet("perishable")]
+        public IActionResult ObtenerProductosPerecederos([FromQuery] int empresa)
+        {
+            var productos = _companyProductsHandler.ObtenerProductosPerecederos(empresa);
+            if (productos == null || !productos.Any())
+            {
+                return Ok();
+            }
+            return Ok(productos);
+        }
+
+    }
+}
