e987faca9e959a5e236e6716a0bff1fe4f97f605
Andre Salas <andre.salaschinchilla@ucr.ac.cr>
2024-09-30 19:55:02 -0600

    Removed unneeded parameter

--- backend/Controllers/registerUserController.cs
+++ backend/Controllers/registerUserController.cs
@@ -25,7 +25,7 @@ namespace backend.Controllers
                 if (data == null) return BadRequest();
                 int IDUser = this.userHandler.addProfile(data);
                 this.userHandler.addUser(data, IDUser);
-                int IDAddr = this.userHandler.addAddr(data, IDUser);
+                int IDAddr = this.userHandler.addAddr(data);
                 this.userHandler.addReferencesAddr(IDUser, IDAddr);
                 return Ok("User registered correctly");
             }
