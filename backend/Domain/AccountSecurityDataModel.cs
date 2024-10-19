d0c96f49b459da2ca58d8cc0b7f0214665cc80bb
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-10-18 21:57:05 -0600

    Changed the name of the directories according to the architecture that is going to be used.

---
+++ backend/Domain/AccountSecurityDataModel.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models
+{
+    public class AccountSecurityDataModel
+    {
+        public string password { get; set; }
+
+        public string userId {  get; set; }
+
+        public string securityCode { get; set; }
+
+        public DateTime dateTimeLastCode { get; set; }
+    }
+}
