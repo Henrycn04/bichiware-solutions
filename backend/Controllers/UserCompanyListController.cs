6f435aea518c511e31a8d46816914dd037ffa589
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-10-18 21:09:31 -0600

    Revert "Changed the name of the  directories of the backend code according to the architecture that is going to be used."
    This reverts commit d47b8fcca674d93e471a361aa37a6734fb4f5b4d.

---
+++ backend/Controllers/UserCompanyListController.cs
@@ -0,0 +1,79 @@
+﻿using backend.Handlers;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserCompanyListController : Controller
+    {
+        private readonly UserCompanyListHandler handler;
+        private const int ADMINISTRATIVE_ROLE = 3;
+        private const int ENTREPRENEUR_ROLE = 2;
+        private const int CUSTOMER_ROLE = 1;
+
+
+        public UserCompanyListController()
+        {
+            this.handler = new UserCompanyListHandler();
+        }
+
+
+        [HttpGet]
+        async public Task<ActionResult<List<UserModel>>> GetUsersList(string userId)
+        {
+            try
+            {
+                int userType = handler.GetUserType(userId);
+                if (userType == ADMINISTRATIVE_ROLE)
+                {
+                    var users = this.handler.RequestAllUsers();
+                    return users;
+                }
+                else if (userType == ENTREPRENEUR_ROLE)
+                {
+                    var users = this.handler.RequestSubordinates(userId);
+                    return users;
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status403Forbidden, "Access Denied");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error at sending users data");
+            }
+        }
+
+        [HttpGet]
+        async public Task<ActionResult<List<CompanyProfileModel>>> GetCompanies(string userId)
+        {
+            try
+            {
+                int userType = handler.GetUserType(userId);
+                if (userType == ADMINISTRATIVE_ROLE)
+                {
+                    var companies = this.handler.RequestAllCompanies();
+                    return companies;
+                }
+                else if (userType == ENTREPRENEUR_ROLE)
+                {
+                    var companies = this.handler.RequestOwnedCompanies(userId);
+                    return companies;
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status403Forbidden, "Access Denied");
+                }
+            }
+            catch (Exception ex1)
+            {
+                Console.WriteLine(ex1.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error at sending companies data");
+            }
+        }
+    }
+}
