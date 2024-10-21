658afe39608789807c2ff303c92ee224a2c1ae3d
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-10-21 01:38:30 -0600

    Created the Orders command to call the Orders handler and make some validations.

---
+++ backend/Application/OrdersCommand.cs
@@ -0,0 +1,30 @@
+﻿using backend.Handlers;
+using backend.Models;
+
+namespace backend.Commands
+{
+    public class OrdersCommand
+    {
+        private readonly OrdersHandler _ordersHandler;
+
+        public OrdersCommand()
+        {
+            _ordersHandler = new OrdersHandler();
+        }
+
+        public List<OrdersModel> getOrderData()
+        {
+            int amountOfOrders = this._ordersHandler.getAmountOfOrders();
+            if (amountOfOrders > 0)
+            {
+                List<OrdersModel> orders = new List<OrdersModel>();
+                orders = this._ordersHandler.getOrdersData();
+                return orders;
+            } else
+            {
+                return new List<OrdersModel>();
+            }
+        }
+
+    }
+}
