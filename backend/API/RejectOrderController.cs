52d5b26183c1fb76f93a08d9a0334b765ecdc7b8
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-10-23 16:49:04 -0600

    Created a controller that makes an endpoint to consume from the frontend code.

---
+++ backend/API/RejectOrderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Queries;
+using backend.Services;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RejectOrderController : Controller
+    {
+        private readonly RejectOrderQuery _rejectOrderQuery;
+
+        public RejectOrderController()
+        {
+            this._rejectOrderQuery = new RejectOrderQuery();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> RejectOrder([FromBody] OrdersModel orderModel)
+        {
+            Console.WriteLine(orderModel.OrderID);
+            int orderID = orderModel.OrderID;
+            try
+            {
+                int rowsAffected = this._rejectOrderQuery.RejectOrder(orderID);
+
+                if (rowsAffected > 0)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound(new { message = "This order does not exists." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Error rejecting the order.", error = ex.Message });
+            }
+        }
+
+
+    }
+}
