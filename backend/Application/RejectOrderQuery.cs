f0d2bd362badf2986ba859e1559ddb9a9dc41932
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-10-23 16:51:11 -0600

    Created a Query that calls a Handler that makes the query that changes the order status to rejected.

---
+++ backend/Application/RejectOrderQuery.cs
@@ -0,0 +1,40 @@
+﻿using backend.Handlers;
+
+namespace backend.Queries
+{
+    public class RejectOrderQuery
+    {
+        private readonly RejectOrderHandler _rejectOrderHandler;
+
+        public RejectOrderQuery()
+        {
+            this._rejectOrderHandler = new RejectOrderHandler();
+        }
+
+        public int RejectOrder(int orderID)
+        {
+            Console.WriteLine(orderID);
+
+            if (checkIfOrderExists(orderID))
+            {
+                int rows = this._rejectOrderHandler.RejectOrder(orderID);
+                return rows;
+            } else
+            {
+                return 0;
+            }
+        }
+
+        private bool checkIfOrderExists(int orderID)
+        {
+            if (this._rejectOrderHandler.CheckIfOrderExists(orderID) == 1)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ backend/Infrastructure/RejectOrderHandler.cs
@@ -0,0 +1,44 @@
+﻿using backend.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace backend.Handlers
+{
+    public class RejectOrderHandler
+    {
+        private SqlConnection _connection;
+        private string _routeConnection;
+        public RejectOrderHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _routeConnection = builder.Configuration.GetConnectionString("BichiwareSolutionsContext");
+            _connection = new SqlConnection(_routeConnection);
+        }
+
+        public int CheckIfOrderExists(int orderID)
+        {
+            Console.WriteLine(orderID);
+
+            string query = "SELECT COUNT(*) FROM Orders WHERE OrderID = @OrderID";
+            SqlCommand commandForQuery = new SqlCommand(query, _connection);
+            commandForQuery.Parameters.AddWithValue("@OrderID", orderID);
+            _connection.Open();
+            int order = (int)commandForQuery.ExecuteScalar();
+            _connection.Close();
+            return order;
+        }
+
+        public int RejectOrder(int orderID)
+        {
+            Console.WriteLine(orderID);
+
+            string query = "UPDATE [dbo].[Orders] SET OrderStatus = 3 WHERE OrderID = @OrderID";
+            SqlCommand commandForQuery = new SqlCommand(query, _connection);
+            commandForQuery.Parameters.AddWithValue("@OrderID", orderID);
+            _connection.Open();
+            int rows = commandForQuery.ExecuteNonQuery();
+            _connection.Close();
+            return rows;
+        }
+    }
+}
