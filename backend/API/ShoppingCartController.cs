fe11f51a9430590a7d80fa6c60a0e4ddef19c92d
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-10-24 17:11:39 -0600

    The shopping cart API is modified to work with the command that removes products from the cart.

--- backend/API/ShoppingCartController.cs
+++ backend/API/ShoppingCartController.cs
@@ -12,11 +12,13 @@ namespace backend.API
     {
         private readonly AddProductToCartCommandHandler _addHandler;
         private readonly GetAllCartProductsHandler _getAllHandler;
+        private readonly DeleteProductFromCartHandler _deleteHandler;
 
         public ShoppingCartController()
         {
             _addHandler = new AddProductToCartCommandHandler();
             _getAllHandler = new GetAllCartProductsHandler();
+            _deleteHandler = new DeleteProductFromCartHandler();
         }
 
         [HttpGet("getAllCartProducts/{userID}")]
@@ -40,6 +42,16 @@ namespace backend.API
             var addProductCommand = new AddProductToCartCommand(_addHandler);
 
             if (!await addProductCommand.Execute(cartProduct))
+                return Ok("Invalid product data or insufficient stock.");
+
+            return Ok("Product added to cart successfully.");
+        }
+        [HttpPost("delete")]
+        public async Task<IActionResult> DeleteProductFromCart([FromBody] DeleteCartProductModel cartProduct)
+        {
+            var deleteProductCommand = new DeleteProductFromCartCommand(_deleteHandler);
+
+            if (!await deleteProductCommand.Execute(cartProduct))
                 return Ok("Invalid product data or insufficient stock.");
 
             return Ok("Product added to cart successfully.");
