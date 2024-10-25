3a35f4dc1b4656593eed7bf2b4aa70338c51fc29
Andre Salas <andre.salaschinchilla@ucr.ac.cr>
2024-10-25 16:51:13 -0600

    Handler for getting Products finished along with 2 procedures for communicating with the database

--- ScriptsDatabase/6-ProceduresOrFunctions.sql
+++ ScriptsDatabase/6-ProceduresOrFunctions.sql
@@ -34,3 +34,33 @@ CREATE PROCEDURE UpdateProfileData
 	WHERE UserID = @UID;
 END;
 GO
+
+CREATE PROCEDURE FindOrderedPerishables
+	@OID int
+	AS
+	BEGIN
+	SELECT
+        op.ProductName,
+		(op.ProductPrice * op.Quantity) AS Cost,
+		op.Quantity,
+		pp.CompanyID
+    FROM OrderedPerishable op
+    INNER JOIN PerishableProduct pp ON op.ProductID = pp.ProductID
+	WHERE op.OrderID = @OID;
+END;
+GO
+
+CREATE PROCEDURE FindOrderedNonPerishables
+    @OID int
+	AS
+	BEGIN
+	SELECT
+		onp.ProductName,
+		(onp.ProductPrice * onp.Quantity) AS Cost,
+		onp.Quantity,
+		np.CompanyID
+	FROM OrderedNonPerishable onp
+	INNER JOIN NonPerishableProduct np ON onp.ProductID = np.ProductID
+	WHERE onp.OrderID = @OID;
+END;
+GO
--- backend/Infrastructure/OrderedProductHandler.cs
+++ backend/Infrastructure/OrderedProductHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Metrics;
 using backend.Domain;
 
 namespace backend.Infrastructure
@@ -15,8 +17,8 @@ namespace backend.Infrastructure
         }
 
         public List<OrderedProductModel> GetProducts(int orderID) {
-            List<OrderedProductModel> perishables = this.GetPerishable(orderID);
-            List<OrderedProductModel> nonPerishables = this.GetNonPerishable(orderID);
+            List<OrderedProductModel> perishables = this.GetProducts(orderID, "FindOrderedPerishables");
+            List<OrderedProductModel> nonPerishables = this.GetProducts(orderID, "FindOrderedNonPerishables");
             List<OrderedProductModel> fullList = new List<OrderedProductModel>();
             for (int i = 0; i < perishables.Count; i++) fullList.Add(perishables[i]);
             for(int i = 0;i < nonPerishables.Count; i++) fullList.Add(nonPerishables[i]);
@@ -24,19 +26,38 @@ namespace backend.Infrastructure
             return fullList;
         }
 
-        private List<OrderedProductModel> GetPerishable(int orderID)
+        private List<OrderedProductModel> GetProducts(int orderID, string procedure)
         {
-            // TODO Finish method
             List<OrderedProductModel> productList = new List<OrderedProductModel>();
+            var commandGetter = new SqlCommand(procedure, _connection);
+            commandGetter.CommandType = CommandType.StoredProcedure;
+            string name;
+            float price;
+            int quantity, companyID;
+            commandGetter.Parameters.AddWithValue("@OID", orderID);
+            _connection.Open();
+            var reader = commandGetter.ExecuteReader();
+            while (reader.Read())
+            {
+                name = reader["ProductName"].ToString();
+                price = float.Parse(reader["Cost"].ToString());
+                quantity = Int32.Parse(reader["Quantity"].ToString());
+                companyID = Int32.Parse(reader["CompanyID"].ToString());
+                productList.Add(CreateModel(name, price, quantity, companyID));
+            }
+            _connection.Close();
             return productList;
         }
 
-        private List<OrderedProductModel> GetNonPerishable(int orderID)
+        private OrderedProductModel CreateModel(string name, float price,
+            int quantity, int companyID)
         {
-            // TODO Finish method
-            List<OrderedProductModel> productList = new List<OrderedProductModel>();
-            return productList;
+            OrderedProductModel model = new OrderedProductModel();
+            model.productName = name;
+            model.totalPrice = price;
+            model.quantity = quantity;
+            model.companyID = companyID;
+            return model;
         }
-
     }
 }
