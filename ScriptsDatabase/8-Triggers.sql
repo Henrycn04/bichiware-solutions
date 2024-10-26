df97730584a477c355477b3992a7b5703debe02b
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-10-25 21:29:20 -0600

    Changes to resolve conflicts in the merge.

--- ScriptsDatabase/8-Triggers.sql
+++ ScriptsDatabase/8-Triggers.sql
@@ -1,3 +1,29 @@
+CREATE TRIGGER trigger_CompanyName_PerishableProduct
+ON PerishableProduct
+AFTER INSERT
+AS
+BEGIN
+    UPDATE pp
+    SET pp.CompanyName = c.CompanyName
+    FROM PerishableProduct pp
+    INNER JOIN INSERTED i ON pp.ProductID = i.ProductID
+    INNER JOIN Company c ON i.CompanyID = c.CompanyID;
+END;
+GO
+
+CREATE TRIGGER trigger_CompanyName_NonPerishableProduct
+ON NonPerishableProduct
+AFTER INSERT
+AS
+BEGIN
+    UPDATE npp
+    SET npp.CompanyName = c.CompanyName
+    FROM NonPerishableProduct npp
+    INNER JOIN INSERTED i ON npp.ProductID = i.ProductID
+    INNER JOIN Company c ON i.CompanyID = c.CompanyID;
+END;
+GO
+
 GO
 CREATE TRIGGER Trigger_Rejected_Orders
 ON Orders
