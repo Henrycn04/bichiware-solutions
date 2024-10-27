2b80ed83687826d74f7ac5049c365da28c6d0dc3
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-10-26 21:28:30 -0600

    The initial structure of the backend that provides the user's address list has been modified.

--- backend/Domain/AddressModel.cs
+++ backend/Domain/AddressModel.cs
@@ -2,9 +2,12 @@
 {
     public class AddressModel
     {
+        public int AddressID { get; set; }
         public string Province { get; set; }
         public string Canton {  get; set; }
         public string District { get; set; }
         public string Exact { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; } 
     }
 }
