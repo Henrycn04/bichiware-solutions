7a6b3bf7efc0299896b7f1714dd2ffab30c3857b
JoseMarioCastroChanto <jose.castrochanto@ucr.ac.cr>
2024-10-26 23:35:07 -0600

    Add triggers for mantain stock and reserved units updated

--- ScriptsDatabase/7-Triggers.sql
+++ ScriptsDatabase/7-Triggers.sql
@@ -23,3 +23,27 @@ BEGIN
     INNER JOIN Company c ON i.CompanyID = c.CompanyID;
 END;
 GO
+CREATE TRIGGER trg_UpdateStockOnInsert
+ON OrderedPerishable
+AFTER INSERT
+AS
+BEGIN
+    UPDATE Delivery
+    SET ReservedUnits = ReservedUnits + inserted.Quantity
+    FROM Delivery
+    INNER JOIN inserted ON Delivery.ProductID = inserted.ProductID
+        AND Delivery.BatchNumber = inserted.BatchNumber;
+END;
+GO
+
+CREATE TRIGGER trg_UpdateStockOnInsertNonPerishable
+ON OrderedNonPerishable
+AFTER INSERT
+AS
+BEGIN
+    UPDATE NonPerishableProduct
+    SET Stock = Stock - inserted.Quantity
+    FROM NonPerishableProduct
+    INNER JOIN inserted ON NonPerishableProduct.ProductID = inserted.ProductID;
+END;
+GO
