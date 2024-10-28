d6e82f9be923aa45507262a4f97906c5f113895b
Andre Salas <andre.salaschinchilla@ucr.ac.cr>
2024-10-27 20:39:27 -0600

    Small simplification of a database script

--- ScriptsDatabase/3-AddressModification.sql
+++ ScriptsDatabase/3-AddressModification.sql
@@ -1,19 +1,5 @@
 ALTER TABLE dbo.Address
-	ADD Latitude DECIMAL(13, 10) NOT NULL
+	ADD Latitude DECIMAL(13, 10) NOT NULL DEFAULT (0) CHECK (Latitude > -90 AND Latitude < 90)
 	
 ALTER TABLE dbo.Address
-	ADD Longitude DECIMAL (13, 10) NOT NULL
-
-GO
-
-ALTER TABLE dbo.Address
-	ADD CONSTRAINT LatitudeDefaultValue DEFAULT(0) FOR Latitude
-
-ALTER TABLE dbo.Address
-	ADD CONSTRAINT LongitudeDefaultValue DEFAULT(0) FOR Longitude
-
-ALTER TABLE dbo.Address
-	ADD CONSTRAINT LatitudeLimits CHECK (Latitude > -90 AND Latitude < 90)
-
-ALTER TABLE dbo.Address
-	ADD CONSTRAINT LongitudeLimits CHECK (Longitude > -180 AND Longitude < 180)
+	ADD Longitude DECIMAL (13, 10) NOT NULL DEFAULT (0) CHECK (Longitude > -180 AND Longitude < 180)
