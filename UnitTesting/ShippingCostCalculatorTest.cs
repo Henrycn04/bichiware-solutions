b6bf4fa7f7f8e44f7c92617a92db6f8ca5f781e1
Daniel Mora Rodriguez <dmorarod16@gmail.com>
2024-10-31 18:04:57 -0600

    Added last unit tests for the Sum of product weights and get order products methods

--- UnitTesting/ShippingCostCalculatorTest.cs
+++ UnitTesting/ShippingCostCalculatorTest.cs
@@ -1,5 +1,6 @@
 ﻿using backend.Models;
 using backend.Infrastructure;
+using backend.Domain;
 
 namespace UnitTesting
 {
@@ -14,7 +15,7 @@ namespace UnitTesting
         }
 
         [Test]
-        public void NegativeMassTest()
+        public void CalculateShippingNegativeMassTest()
         {
             // Arrange
             PhysicalAddress address = new PhysicalAddress()
@@ -30,7 +31,7 @@ namespace UnitTesting
         }
 
         [Test]
-        public void UnrealisticAddress()
+        public void CalculateShippingUnrealisticAddressTest()
         {
             // Arrange
             PhysicalAddress address = new PhysicalAddress()
@@ -46,7 +47,7 @@ namespace UnitTesting
         }
 
         [Test]
-        public void AddressIsOutsideCostaRica()
+        public void CalculateShippingAddressIsOutsideCostaRicaTest()
         {
             // Arrange
             PhysicalAddress address = new PhysicalAddress()
@@ -62,7 +63,7 @@ namespace UnitTesting
         }
 
         [Test]
-        public void AddressIsNull()
+        public void CalculateShippingAddressIsNullTest()
         {
             // Arrange
             PhysicalAddress address = null;
@@ -74,7 +75,7 @@ namespace UnitTesting
         }
 
         [Test]
-        public void MassIsTooSmall()
+        public void CalculateShippingMassIsTooSmallTest()
         {
             // Arrange
             PhysicalAddress address = new PhysicalAddress()
@@ -87,6 +88,61 @@ namespace UnitTesting
             double cost = calculator.CalculateShippingCost(address, mass);
             // Assert
             Assert.AreEqual(cost, 3000);
+        }
+
+        [Test]
+        public void SumOrderProductsNegativeOrderTest()
+        {
+            // Arrange
+            int orderId = -1;
+            // Act
+            double weight = calculator.SumOrderProductsWeight(orderId);
+            // Assert
+            Assert.AreEqual(weight, 0);
+        }
+
+        [Test]
+        public void SumOrderProductsUnexistentOrderTest()
+        {
+            // Arrange
+            int orderId = 83489494;
+            // Act
+            double weight = calculator.SumOrderProductsWeight(orderId);
+            // Assert
+            Assert.AreEqual(weight, 0);
+        }
+
+        [Test]
+        public void SumOrderProductsBoundaryTest()
+        {
+            // Arrange
+            int orderId = 2147483647;
+            // Act
+            double weight = calculator.SumOrderProductsWeight(orderId);
+            // Assert
+            Assert.AreEqual(weight, 0);
+        }
+
+        [Test]
+        public void SumOrderNormalOrderTest()
+        {
+            // Arrange
+            int orderId = 1;
+            // Act
+            double weight = calculator.SumOrderProductsWeight(orderId);
+            // Assert
+            Assert.Greater(weight, 0);
+        }
+
+        [Test]
+        public void GetOrderProductsNegativeOrderTest()
+        {
+            // Arrange
+            int orderId = -1;
+            // Act
+            List<OrderProductModel> products = calculator.GetOrderProducts(orderId);
+            // Assert
+            Assert.IsEmpty(products);
         }
     }
 }
