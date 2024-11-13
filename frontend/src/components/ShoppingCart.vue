6aa82db85064d8cd374050cedbb43b23e91c5283
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-11-13 13:32:15 -0600

    Add design to the landing page for the customer dashboard, and also add the component that displays active orders.

--- frontend/src/components/ShoppingCart.vue
+++ frontend/src/components/ShoppingCart.vue
@@ -137,7 +137,7 @@
                 this.$router.push('/orderconfirmation');
             },
             getAllCartProducts() {
-                axios.get(this.$backendAddress +"api/ShoppingCart/getAllCartProducts/${this.userCredentials.userId}")
+                axios.get(`${this.$backendAddress}api/ShoppingCart/getAllCartProducts/${this.userCredentials.userId}`)
                 .then((response) => {
                     if (typeof response.data === "string") {
                         console.warn(response.data);
