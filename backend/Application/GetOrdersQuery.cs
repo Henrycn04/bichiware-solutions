13cfe83f74ec683618d539fc69c851782bfc696e
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-11-13 13:35:10 -0600

    Add the query that makes requests to the handler for active orders.

---
+++ backend/Application/GetOrdersQuery.cs
@@ -0,0 +1,29 @@
+﻿using backend.Domain;
+using backend.Handlers;
+using backend.Infrastructure;
+using backend.Models;
+
+namespace backend.Commands
+{
+    public class GetOrdersQuery
+    {
+        private readonly GetOrdersHandler _handler;
+
+        public GetOrdersQuery(GetOrdersHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public async Task<List<UserOrdersModel>> Execute(int userId)
+        {
+            if (!await _handler.UserExists(userId))
+            {
+                return null;
+            }
+
+            var products = await _handler.GetAllOrdersWithProducts(userId);
+
+            return products;
+        }
+    }
+}
