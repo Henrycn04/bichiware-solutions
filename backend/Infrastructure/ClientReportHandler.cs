50823fdbf8bf83fdfcd6b24d53627902fbb3b2d7
Andre Salas <andre.salaschinchilla@ucr.ac.cr>
2024-11-18 17:42:06 -0600

    Command generator added for use on client reports

--- backend/Infrastructure/ClientReportHandler.cs
+++ backend/Infrastructure/ClientReportHandler.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Xml.Linq;
 using backend.Domain;
+using MailKit.Search;
 
 namespace backend.Infrastructure
 {
@@ -16,137 +17,24 @@ namespace backend.Infrastructure
             var builder = WebApplication.CreateBuilder();
             _routeConnection = builder.Configuration.GetConnectionString("RegisterUser");
             _connection = new SqlConnection(_routeConnection);
-            query = "EXEC ClientGetOrders @OrderStatus";
+            query = "EXEC ClientGetOrders @OrderStatus = @OS";
         }
 
-        public List<ClientReportResponseModel> GetCompletedReport (ClientReportRequestModel request)
-        {
-            var commandForQuery = new SqlCommand(this.query, _connection);
-            List<ClientReportResponseModel> reportList = GenerateCompletedReports(commandForQuery);
-            return reportList;
-        }
-
-        private List<ClientReportResponseModel> GenerateCompletedReports (SqlCommand commandForQuery)
-        {
-            List<ClientReportResponseModel> orderList = new List<ClientReportResponseModel>();
-            string orderID, AllCompanies, Quantity, CreationDate, SentDate,
-                DeliveredDate, ProductCost, DeliveryCost, Total;
-            _connection.Open();
-            var reader = commandForQuery.ExecuteReader();
-            while (reader.Read())
-            {
-                orderID = reader["OrderID"].ToString();
-                AllCompanies = reader["AllCompanies"].ToString();
-                Quantity = reader["Quantity"].ToString();
-                CreationDate = reader["CreationDate"].ToString();
-                SentDate = reader["SentDate"].ToString();
-                DeliveredDate = reader["DeliveredDate"].ToString();
-                ProductCost = reader["ProductCost"].ToString();
-                DeliveryCost = reader["ShippingCost"].ToString();
-                Total = reader["Total"].ToString();
-                orderList.Add(CreateCompletedModel(orderID, AllCompanies, 
-                    Quantity, CreationDate, SentDate, DeliveredDate, 
-                    ProductCost, DeliveryCost, Total));
-            }
-            _connection.Close();
-            return orderList;
-        }
-
-        private ClientReportResponseModel CreateCompletedModel(
-            string orderID, string AllCompanies, string Quantity,
-            string CreationDate, string SentDate, string DeliveredDate, 
-            string ProductCost, string DeliveryCost, string Total)
-        {
-            ClientReportResponseModel report = new ClientReportResponseModel();
-            report.OrderID = Int32.Parse(orderID);
-            report.Companies = AllCompanies;
-            report.Quantity = Int32.Parse(Quantity);
-            report.CreationDate = CreationDate;
-            report.SentDate = SentDate;
-            report.DeliveredDate = DeliveredDate;
-            report.ProductCost = Double.Parse(ProductCost);
-            report.DeliveryCost = Double.Parse(DeliveryCost);
-            report.TotalCost = Double.Parse(Total);
-            return report;
-        }
-
-        public List<ClientReportResponseModel> GetCancelledReport (ClientReportRequestModel request)
-        {
-            var commandForQuery = new SqlCommand(this.query, _connection);
-            List<ClientReportResponseModel> reportList = this.GenerateDeletedReports(commandForQuery);
-
-            return reportList;
-        }
-
-        private List<ClientReportResponseModel> GenerateDeletedReports(SqlCommand commandForQuery)
-        {
-            List<ClientReportResponseModel> orderList = new List<ClientReportResponseModel>();
-            string orderID, AllCompanies, Quantity, CreationDate, CancelledDate,
-                CancelledBy, ProductCost, DeliveryCost, Total;
-            _connection.Open();
-            var reader = commandForQuery.ExecuteReader();
-            while (reader.Read())
-            {
-                orderID = reader["OrderID"].ToString();
-                AllCompanies = reader["AllCompanies"].ToString();
-                Quantity = reader["Quantity"].ToString();
-                CreationDate = reader["CreationDate"].ToString();
-                CancelledDate = reader["CancelledDate"].ToString();
-                CancelledBy = reader["CancelledBy"].ToString();
-                ProductCost = reader["ProductCost"].ToString();
-                DeliveryCost = reader["ShippingCost"].ToString();
-                Total = reader["Total"].ToString();
-                orderList.Add(CreateDeletedModel(orderID, AllCompanies,
-                    Quantity, CreationDate, CancelledDate, CancelledBy,
-                    ProductCost, DeliveryCost, Total));
-            }
-            _connection.Close();
-            return orderList;
-        }
-
-        private ClientReportResponseModel CreateDeletedModel(
-            string orderID, string AllCompanies, string Quantity,
-            string CreationDate, string CancelledDate, string CancelledBy,
-            string ProductCost, string DeliveryCost, string Total)
-        {
-            ClientReportResponseModel report = new ClientReportResponseModel();
-            report.OrderID = Int32.Parse(orderID);
-            report.Companies = AllCompanies;
-            report.Quantity = Int32.Parse(Quantity);
-            report.CreationDate = CreationDate;
-            report.CancelledDate = CancelledDate;
-            report.CancelledBy = Int32.Parse(CancelledBy);
-            report.ProductCost = Double.Parse(ProductCost);
-            report.DeliveryCost = Double.Parse(DeliveryCost);
-            report.TotalCost = Double.Parse(Total);
-            return report;
-        }
-
-        public List<ClientReportResponseModel> GetCurrentReport(ClientReportRequestModel request)
-        {
-            string localQuery = this.query;
-
-            var commandForQuery = new SqlCommand(localQuery, _connection);
-            commandForQuery.CommandType = CommandType.StoredProcedure;
-            List<ClientReportResponseModel> reportList = this.GenerateCurrentReports(commandForQuery);
-            return reportList;
-        }
-
-        private (string, bool[]) GenerateString(ClientReportRequestModel request) 
+        private (string, bool[]) GenerateString(ClientReportRequestModel request)
         {
             string localQuery = this.query;
             bool[] used = new bool[19];
-            if (request.UserID > 0) 
+            if (request.UserID > 0)
             {
                 localQuery += ", @UID = @UIDEntry";
                 used[0] = true;
             }
-            if (request.CreationStartDate != "") 
+            if (request.CreationStartDate != "")
             {
                 localQuery += ", @CreationStartDate = @CSDEntry";
                 used[1] = true;
             }
-            if(request.CreationEndDate != "")
+            if (request.CreationEndDate != "")
             {
                 localQuery += ", @CreationEndDate = @CEDEntry";
                 used[2] = true;
@@ -233,6 +121,147 @@ namespace backend.Infrastructure
             }
 
             return (localQuery, used);
+        }
+
+        private SqlCommand GenerateCommand(ClientReportRequestModel request, string query, bool[] used)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@OS", request.RequestType);
+            if (used[0]) command.Parameters.AddWithValue("@UID", request.UserID);
+            if (used[1]) command.Parameters.AddWithValue("@CSDEntry", request.CreationStartDate);
+            if (used[2]) command.Parameters.AddWithValue("@CEDEntry", request.CreationEndDate);
+            if (used[3]) command.Parameters.AddWithValue("@SSDEntry", request.SentStartDate);
+            if (used[4]) command.Parameters.AddWithValue("@SEDEntry", request.SentEndDate);
+            if (used[5]) command.Parameters.AddWithValue("@DSDEntry", request.DeliveredStartDate);
+            if (used[6]) command.Parameters.AddWithValue("@DEDEntry", request.DeliveredEndDate);
+            if (used[7]) command.Parameters.AddWithValue("@CaSDEntry", request.CancelledStartDate);
+            if (used[8]) command.Parameters.AddWithValue("@CaEDEntry", request.CancelledEndDate);
+            if (used[9]) command.Parameters.AddWithValue("@CaByEntry", request.CancelledBy);
+            if (used[10]) command.Parameters.AddWithValue("@minSCEntry", request.minShippingCost);
+            if (used[11]) command.Parameters.AddWithValue("@maxSCEntry", request.maxShippingCost);
+            if (used[12]) command.Parameters.AddWithValue("@minPCEntry", request.minProductCost);
+            if (used[13]) command.Parameters.AddWithValue("@maxPCEntry", request.maxProductCost);
+            if (used[14]) command.Parameters.AddWithValue("@minTCEntry", request.minTotalCost);
+            if (used[15]) command.Parameters.AddWithValue("@maxTCEntry", request.maxTotalCost);
+            if (used[16]) command.Parameters.AddWithValue("@minQEntry", request.minQuantity);
+            if (used[17]) command.Parameters.AddWithValue("@maxQEntry", request.maxQuantity);
+            if (used[18]) command.Parameters.AddWithValue("@OIDEntry", request.orderID);
+
+            return command;
+        }
+
+        public List<ClientReportResponseModel> GetCompletedReport (ClientReportRequestModel request)
+        {
+            (string, bool[]) data = this.GenerateString(request);
+            var commandForQuery = this.GenerateCommand(request, data.Item1, data.Item2);
+            List<ClientReportResponseModel> reportList = GenerateCompletedReports(commandForQuery);
+            return reportList;
+        }
+
+        private List<ClientReportResponseModel> GenerateCompletedReports (SqlCommand commandForQuery)
+        {
+            List<ClientReportResponseModel> orderList = new List<ClientReportResponseModel>();
+            string orderID, AllCompanies, Quantity, CreationDate, SentDate,
+                DeliveredDate, ProductCost, DeliveryCost, Total;
+            _connection.Open();
+            var reader = commandForQuery.ExecuteReader();
+            while (reader.Read())
+            {
+                orderID = reader["OrderID"].ToString();
+                AllCompanies = reader["AllCompanies"].ToString();
+                Quantity = reader["Quantity"].ToString();
+                CreationDate = reader["CreationDate"].ToString();
+                SentDate = reader["SentDate"].ToString();
+                DeliveredDate = reader["DeliveredDate"].ToString();
+                ProductCost = reader["ProductCost"].ToString();
+                DeliveryCost = reader["ShippingCost"].ToString();
+                Total = reader["Total"].ToString();
+                orderList.Add(CreateCompletedModel(orderID, AllCompanies, 
+                    Quantity, CreationDate, SentDate, DeliveredDate, 
+                    ProductCost, DeliveryCost, Total));
+            }
+            _connection.Close();
+            return orderList;
+        }
+
+        private ClientReportResponseModel CreateCompletedModel(
+            string orderID, string AllCompanies, string Quantity,
+            string CreationDate, string SentDate, string DeliveredDate, 
+            string ProductCost, string DeliveryCost, string Total)
+        {
+            ClientReportResponseModel report = new ClientReportResponseModel();
+            report.OrderID = Int32.Parse(orderID);
+            report.Companies = AllCompanies;
+            report.Quantity = Int32.Parse(Quantity);
+            report.CreationDate = CreationDate;
+            report.SentDate = SentDate;
+            report.DeliveredDate = DeliveredDate;
+            report.ProductCost = Double.Parse(ProductCost);
+            report.DeliveryCost = Double.Parse(DeliveryCost);
+            report.TotalCost = Double.Parse(Total);
+            return report;
+        }
+
+        public List<ClientReportResponseModel> GetCancelledReport (ClientReportRequestModel request)
+        {
+            (string, bool[]) data = this.GenerateString(request);
+            var commandForQuery = this.GenerateCommand(request, data.Item1, data.Item2);
+            List<ClientReportResponseModel> reportList = this.GenerateDeletedReports(commandForQuery);
+
+            return reportList;
+        }
+
+        private List<ClientReportResponseModel> GenerateDeletedReports(SqlCommand commandForQuery)
+        {
+            List<ClientReportResponseModel> orderList = new List<ClientReportResponseModel>();
+            string orderID, AllCompanies, Quantity, CreationDate, CancelledDate,
+                CancelledBy, ProductCost, DeliveryCost, Total;
+            _connection.Open();
+            var reader = commandForQuery.ExecuteReader();
+            while (reader.Read())
+            {
+                orderID = reader["OrderID"].ToString();
+                AllCompanies = reader["AllCompanies"].ToString();
+                Quantity = reader["Quantity"].ToString();
+                CreationDate = reader["CreationDate"].ToString();
+                CancelledDate = reader["CancelledDate"].ToString();
+                CancelledBy = reader["CancelledBy"].ToString();
+                ProductCost = reader["ProductCost"].ToString();
+                DeliveryCost = reader["ShippingCost"].ToString();
+                Total = reader["Total"].ToString();
+                orderList.Add(CreateDeletedModel(orderID, AllCompanies,
+                    Quantity, CreationDate, CancelledDate, CancelledBy,
+                    ProductCost, DeliveryCost, Total));
+            }
+            _connection.Close();
+            return orderList;
+        }
+
+        private ClientReportResponseModel CreateDeletedModel(
+            string orderID, string AllCompanies, string Quantity,
+            string CreationDate, string CancelledDate, string CancelledBy,
+            string ProductCost, string DeliveryCost, string Total)
+        {
+            ClientReportResponseModel report = new ClientReportResponseModel();
+            report.OrderID = Int32.Parse(orderID);
+            report.Companies = AllCompanies;
+            report.Quantity = Int32.Parse(Quantity);
+            report.CreationDate = CreationDate;
+            report.CancelledDate = CancelledDate;
+            report.CancelledBy = Int32.Parse(CancelledBy);
+            report.ProductCost = Double.Parse(ProductCost);
+            report.DeliveryCost = Double.Parse(DeliveryCost);
+            report.TotalCost = Double.Parse(Total);
+            return report;
+        }
+
+        public List<ClientReportResponseModel> GetCurrentReport(ClientReportRequestModel request)
+        {
+            (string, bool[]) data = this.GenerateString(request);
+            var commandForQuery = this.GenerateCommand(request, data.Item1, data.Item2);
+            List<ClientReportResponseModel> reportList = this.GenerateCurrentReports(commandForQuery);
+            return reportList;
         }
 
         private List<ClientReportResponseModel> GenerateCurrentReports(SqlCommand commandForQuery)
