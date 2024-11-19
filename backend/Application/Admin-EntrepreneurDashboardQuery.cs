5a10edf1b4fee1989e6177cb58c3dee88769a87c
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-11-18 20:50:42 -0600

    Created a Query to make validations before requesting data to the database

---
+++ backend/Application/Admin-EntrepreneurDashboardQuery.cs
@@ -0,0 +1,46 @@
+﻿using backend.Handlers;
+using backend.Models;
+
+namespace backend.Queries
+{
+    public class Admin_EntrepreneurDashboardQuery
+    {
+        private readonly IAdmin_EntrepreneurOrdersHandler _admin_EntrepreneurOrdersHandler;
+
+        public Admin_EntrepreneurDashboardQuery(IAdmin_EntrepreneurOrdersHandler admin_EntrepreneurOrdersHandler)
+        {
+            this._admin_EntrepreneurOrdersHandler = admin_EntrepreneurOrdersHandler;
+        }
+
+        public List<UserOrdersModel> GetOrdersForAdmin()
+        {
+            var orders = this._admin_EntrepreneurOrdersHandler.GetOrdersForAdmin();
+            return orders;
+        }
+
+        public  List<EntrepreneurOrdersModel> GetOrdersForEntrepreneur(int userID)
+        {
+            List<EntrepreneurOrdersModel> orders = new List<EntrepreneurOrdersModel>();
+            if (CheckIfUserHasCompanies(userID) && checkValidityOfNumber(userID) && CheckIfUserExists(userID))
+            {
+                orders = this._admin_EntrepreneurOrdersHandler.GetOrdersForEntrepreneur(userID);
+            }
+            return orders;
+        }
+
+        private bool CheckIfUserExists(int userID)
+        {
+            return (this._admin_EntrepreneurOrdersHandler.CheckIfUserExists(userID) > 0);
+        }
+
+        private bool CheckIfUserHasCompanies(int userID)
+        {
+            return (this._admin_EntrepreneurOrdersHandler.CheckIfUserHasCompanies(userID) > 0);
+        }
+
+        private bool checkValidityOfNumber(int userID)
+        {
+            return (userID > 0);
+        }
+    }
+}
