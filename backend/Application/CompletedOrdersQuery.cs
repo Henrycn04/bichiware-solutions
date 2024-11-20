71112754221eeea8f06343956e7af16bb205fbed
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-11-20 17:32:08 -0600

    Added the query to verify user login and permissions required to retrieve all completed orders.

---
+++ backend/Application/CompletedOrdersQuery.cs
@@ -0,0 +1,44 @@
+﻿using backend.Domain;
+using backend.Handlers;
+using backend.Infrastructure;
+using backend.Models;
+
+namespace backend.Commands
+{
+    public class CompletedOrdersQuery
+    {
+        private readonly CompletedOrdersReportHandler _handler;
+
+        public CompletedOrdersQuery()
+        {
+            _handler = new CompletedOrdersReportHandler();
+        }
+        public CompletedOrdersQuery(CompletedOrdersReportHandler handler)
+        {
+            _handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public async Task<List<CompletedOrdersModel>> Execute(FiltersCompletedOrdersModel filter)
+        {
+            if (!await _handler.UserExists(filter.UserID))
+            {
+                throw new ArgumentException("UserID does not exist.");
+            }
+
+            int userType = await _handler.UserIsAdminOrEntrepeneur(filter.UserID);
+            if (userType != 2 && userType != 3) 
+            {
+                throw new UnauthorizedAccessException("User is not authorized to view orders.");
+            }
+
+            if (userType == 2 && filter.CompanyID.HasValue && !await _handler.CompanyExists(filter.CompanyID.Value))
+            {
+                throw new ArgumentException("CompanyID does not exist.");
+            }
+
+            var orders = await _handler.GetOrdersByFilterAsync(filter);
+
+            return orders;
+        }
+    }
+}
