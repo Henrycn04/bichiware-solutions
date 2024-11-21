46142d7fd3945f21758718c2a34fd42ad7c128ad
JoseMarioCastroChanto <jose.castrochanto@ucr.ac.cr>
2024-11-20 19:55:55 -0600

    Add method in the controller for total profits report

--- backend/API/ReportsController.cs
+++ backend/API/ReportsController.cs
@@ -3,7 +3,9 @@ using backend.Commands;
 using backend.Domain;
 using backend.Infrastructure;
 using backend.Models;
+using backend.Queries;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
 
 namespace backend.API
 {
@@ -13,9 +15,11 @@ namespace backend.API
     {
 
         private ClientReportQuery clientReportQuery;
-        public ReportsController()
+        private TotalProfitsQuery _totalProfitsReportQuery;
+        public ReportsController(TotalProfitsQuery totalProfitsReportQuery)
         {
             clientReportQuery = new ClientReportQuery();
+            _totalProfitsReportQuery = totalProfitsReportQuery;
         }
         /*
         [HttpGet("getReport/completedOrders/")]
@@ -48,6 +52,23 @@ namespace backend.API
             catch (Exception e)
             {
                 return BadRequest(e.Message);
+            }
+        }
+        [HttpPost("total-profits")]
+        public async Task<IActionResult> GetTotalProfits([FromBody] TotalProftsRequestModel request)
+        {
+            try
+            {
+                var result = await _totalProfitsReportQuery.GetTotalProfits(request);
+                return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Ocurrió un error inesperado.", Details = ex.Message });
             }
         }
 
