89c76a8425e4cbefbf6ccb11aa130997602e1415
JoseMarioCastroChanto <jose.castrochanto@ucr.ac.cr>
2024-11-20 19:56:40 -0600

    Add the models for request and response for total profits report

---
+++ backend/Domain/TotalProfitsResponseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Domain
+{
+    public class TotalProfitsResponseModel
+    {
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal TotalShippingCost { get; set; }
+        public decimal TotalOrderPrice { get; set; }
+    }
+}
