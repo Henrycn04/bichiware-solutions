03af9b43ed134a9e089528e9e6cdbb114d9f66b2
Bianchini <felipe.bianchini@ucr.ac.cr>
2024-11-21 03:15:00 -0600

    Created a Query to make validations and then ask the handler for the cancelled orders data

--- backend/Application/CancelledOrdersQuery.cs
+++ backend/Application/CancelledOrdersQuery.cs
@@ -1,5 +1,8 @@
 ﻿using backend.Handlers;
 using backend.Models;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata;
 
 namespace backend.Queries
 {
@@ -10,6 +13,45 @@ namespace backend.Queries
         public CancelledOrdersQuery(ICancelledOrdersHandler cancelledOrdersHandler)
         {
             this._cancelledOrdersHandler = cancelledOrdersHandler;
+        }
+
+        public List<CancelledOrdersModel> GetCancelledOrders(FiltersCompletedOrdersModel filter)
+        {
+
+            this.CheckIfUserExists(filter.UserID);
+            this.CheckIfUserIsAdminOrEntrepreneur(filter.UserID);
+            this.CheckIfCompanyExists(filter.CompanyID);
+            return this._cancelledOrdersHandler.GetCancelledOrders(filter);
+        }
+
+        private void CheckIfUserExists(int userID)
+        {
+            if (_cancelledOrdersHandler.CheckIfUserExists(userID) <= 0)
+            {
+                throw new ArgumentException("UserID does not exist.");
+            }
+        }
+
+        private void CheckIfCompanyExists(int? companyID)
+        {
+            if (companyID.HasValue)
+            {
+                int companyIDValue = companyID.Value;
+                if (_cancelledOrdersHandler.CheckIfCompanyExists(companyIDValue) <= 0)
+                {
+                    throw new ArgumentException("CompanyID does not exist.");
+                }
+            }
+        }
+
+        private void CheckIfUserIsAdminOrEntrepreneur(int userID)
+        {
+            int userType = _cancelledOrdersHandler.CheckIfUserIsAdminOrEntrepeneur(userID);
+            if (userType == 1)
+            {
+                throw new UnauthorizedAccessException("User is not authorized to view orders.");
+
+            }
         }
     }
 }
