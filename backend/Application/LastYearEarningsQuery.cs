ba360086766fd741e2e1d363c43ba685178c8adb
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-11-23 14:21:22 -0600

    The query that processes requests for earnings from the past year is designed.

---
+++ backend/Application/LastYearEarningsQuery.cs
@@ -0,0 +1,38 @@
+﻿using backend.Domain;
+using backend.Infrastructure;
+
+namespace backend.Application
+{
+    public class LastYearEarningsQuery
+    {
+        private readonly ILastYearEarningsHandler _handler;
+
+        public LastYearEarningsQuery(ILastYearEarningsHandler handler)
+        {
+            this._handler = handler;
+        }
+
+        public async Task<List<LastYearOrdersModel>> Execute(LastYearEarningsElementsModel filter)
+        {
+            if (!await _handler.UserExists(filter.UserID))
+            {
+                throw new ArgumentException("UserID does not exist.");
+            }
+
+            int userType = await _handler.UserIsAdminOrEntrepeneur(filter.UserID);
+            if (userType != 2 && userType != 3)
+            {
+                throw new UnauthorizedAccessException("User is not authorized to view orders.");
+            }
+
+            if (userType == 2 && filter.CompanyID.HasValue && !await _handler.CompanyExists(filter.CompanyID.Value))
+            {
+                throw new ArgumentException("CompanyID does not exist.");
+            }
+
+            var orders = await _handler.GetOrdersByFilterAsync(filter);
+
+            return orders;
+        }
+    }
+}
