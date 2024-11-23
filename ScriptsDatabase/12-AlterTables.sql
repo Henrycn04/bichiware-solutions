a0730e0febfcde8b055b08c37ee2a77f5250860b
Andre Salas <andre.salaschinchilla@ucr.ac.cr>
2024-11-23 17:49:15 -0600

    Added sets for all Deleted Columns and for Orders´ CreationDate

--- ScriptsDatabase/12-AlterTables.sql
+++ ScriptsDatabase/12-AlterTables.sql
@@ -3,29 +3,58 @@ ALTER TABLE Profile
 ADD Deleted bit DEFAULT 0
 GO
 
+UPDATE Profile
+SET Deleted = 0;
+GO
+
 ALTER TABLE Company
 ADD Deleted bit DEFAULT 0
+GO
+
+UPDATE Company
+SET Deleted = 0;
 GO
 
 ALTER TABLE PerishableProduct
 ADD Deleted bit DEFAULT 0
 GO
 
+UPDATE PerishableProduct
+SET Deleted = 0;
+GO
+
 ALTER TABLE NonPerishableProduct
 ADD Deleted bit DEFAULT 0
+GO
+
+UPDATE NonPerishableProduct
+SET Deleted = 0;
 GO
 
 ALTER TABLE Address
 ADD Deleted bit DEFAULT 0
 GO
 
+UPDATE Address
+SET Deleted = 0;
+GO
+
+
 --Dates for orders (used in reports)
 ALTER TABLE Orders 
 ADD CreationDate DATE DEFAULT GETDATE()
 GO
 
+UPDATE Orders
+SET CreationDate = GETDATE();
+GO
+
 ALTER TABLE Orders 
 ADD CancellationDate DATE 
+GO
+
+UPDATE Orders
+SET CreationDate = GETDATE();
 GO
 
 ALTER TABLE Orders
